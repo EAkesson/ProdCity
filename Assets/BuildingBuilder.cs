2138ba11b5918793571d4c47105e1de99e4040df
EAkesson <mail@eakesson.se>
2020-03-02 22:07:48 +0100

    Started to build so the function can build a block and serveral blocks to a city

--- Assets/BuildingBuilder.cs
+++ Assets/BuildingBuilder.cs
@@ -18,32 +18,38 @@ public class BuildingBuilder : MonoBehaviour
 
     public Vector3 GenerateHouse(int numOfSegments)
     {
+        GameObject houseGameObj = new GameObject("Building" + 56);
+        houseGameObj.transform.position = this.transform.position;
+        houseGameObj.transform.rotation = this.transform.rotation;
+
+        houseGameObj.transform.SetParent(this.transform);
+
         Mathf.Clamp(numOfSegments, minHeight, maxHeight);
         //int numOfSegments = Random.Range(minHeight, maxHeight);
         float totalHeight = 0.0f;
 
         //Spawn base
-        totalHeight += GenerateSegment(groundSegments, totalHeight);
+        totalHeight += GenerateSegment(groundSegments, totalHeight, houseGameObj);
 
         for (int i = 2; i < numOfSegments; i++)
         {
             //Spawn new base part
-            totalHeight += GenerateSegment(middleSegments, totalHeight);
+            totalHeight += GenerateSegment(middleSegments, totalHeight, houseGameObj);
         }
 
         //Spawn roof
-        totalHeight += GenerateSegment(topSegments, totalHeight);
+        totalHeight += GenerateSegment(topSegments, totalHeight, houseGameObj);
 
-        Vector3 houseDim = this.gameObject.GetComponent<MeshFilter>().mesh.bounds.size;
+        Vector3 houseDim = this.gameObject.GetComponentInChildren<MeshFilter>().mesh.bounds.size;
         this.transform.DetachChildren();
         return houseDim;
     }
 
-    float GenerateSegment(GameObject[] segmentPool, float placementHeight)
+    float GenerateSegment(GameObject[] segmentPool, float placementHeight, GameObject houseGo)
     {
         int segmentNum = Random.Range(0, segmentPool.Length-1);
-        GameObject segment = Instantiate(segmentPool[segmentNum], this.transform.position + new Vector3(0, placementHeight, 0), transform.rotation) as GameObject;
-        segment.transform.SetParent(this.transform);
+        GameObject segment = Instantiate(segmentPool[segmentNum], this.transform.position + new Vector3(0, placementHeight, 0), this.transform.rotation) as GameObject;
+        segment.transform.SetParent(houseGo.transform);
 
         float segmentHeight = segment.GetComponentInChildren<MeshFilter>().mesh.bounds.size.y;
         return segmentHeight;
--- Assets/WorldBuilder.cs
+++ Assets/WorldBuilder.cs
@@ -8,11 +8,20 @@ public class WorldBuilder : MonoBehaviour
 {
     public BuildingBuilder BB;
     public bool respawn = false;
+
+    public Vector2Int blockSize = new Vector2Int(3, 3);
+    public Vector2Int cityTileSize = new Vector2Int(50, 50);
+    Vector2Int currentBlockSize = new Vector2Int(0, 0);
+    Vector2Int currentCityTileSize = new Vector2Int(0, 0);
+
+    float roadWidth = 1.5f;
+    float houseSeperation = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
         int height = Random.Range(0, 10);
-        BB.GenerateHouse(height);
+        Vector3 h = BB.GenerateHouse(height);
+        this.transform.Translate(Vector3.forward * (h.z + houseSeperation));
     }
 
     void BuildHouse()
@@ -27,7 +36,26 @@ public class WorldBuilder : MonoBehaviour
             respawn = false;            
             int height = Random.Range(0, 10);
             Vector3 h = BB.GenerateHouse(height);            
-            this.transform.Translate(Vector3.back * h.z);
+            this.transform.Translate(Vector3.forward * (h.z + houseSeperation));
+            currentBlockSize.x++;
+            currentCityTileSize.x++;
+
+            if (currentBlockSize.x >= blockSize.x)
+            {
+                this.transform.Translate(Vector3.forward * (roadWidth));
+                currentBlockSize.x = 0;
+            }
+            if (currentBlockSize.y >= blockSize.y)
+            {
+                //this.transform.Translate(Vector3.left * (roadWidth));
+                currentBlockSize.y = 0;
+            }
+            if (currentCityTileSize.x >= cityTileSize.x)
+            {
+
+            }
+            //this.transform.Translate(Vector3.back * h.z);
+            //this.transform.Translate(Vector3.back * h.z);
         }
     }
 }
